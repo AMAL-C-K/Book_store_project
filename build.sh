75ece48d947c07b68d41b5380e6ec89b722d58ed
AMAL-C-K <amalckg1999@gmail.com>
2026-09-03 16:05:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+username='admin';
+email='admin@gmail.com';
+password='1234';
+User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)
+"
